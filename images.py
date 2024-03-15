170d24b139c20f9db0ec04cfc0eb18d3a9f66d66
AlexWeyhe <161962774+AlexWeyhe@users.noreply.github.com>
2024-03-15 12:48:20 +0100

    Update images.py

--- images.py
+++ images.py
@@ -1,24 +1,233 @@
 import pygame
 
-image1 = pygame.image.load("pictures/e.png")
-image1 = pygame.transform.scale(image1, (150, 150))
-image2 = pygame.image.load("pictures/schwa.png")
-image2 = pygame.transform.scale(image2, (150, 150))
-image3 = pygame.image.load("pictures/ae.png")
-image3 = pygame.transform.scale(image3, (150, 150))
-image4 = pygame.image.load("pictures/dach.png")
-image4 = pygame.transform.scale(image4, (150, 150))
-image5 = pygame.image.load("pictures/hufeisen.png")
-image5 = pygame.transform.scale(image5, (150, 150))
-image6 = pygame.image.load("pictures/reversea.png")
-image6 = pygame.transform.scale(image6, (150, 150))
-image7 = pygame.image.load("pictures/dach_desc.png")
-image7 = pygame.transform.scale(image7, (150, 150))
-image8 = pygame.image.load("pictures/e_desc.png")
-image8 = pygame.transform.scale(image8, (150, 150))
+# CLOSE FRONT VOWELS
+image1 = pygame.image.load("pictures/CFU.png")
+image1_normal = pygame.transform.scale(image1, (150, 150))
+image1_goat = pygame.transform.scale(image1, (90, 90))
+
+image2 = pygame.image.load("pictures/cfunrounded.png")
+image2_normal = pygame.transform.scale(image2, (150, 150))
+image2_goat = pygame.transform.scale(image2, (90, 90))
+
+image3 = pygame.image.load("pictures/CFR.png")
+image3_normal = pygame.transform.scale(image3, (150, 150))
+image3_goat = pygame.transform.scale(image3, (90, 90))
+
+image4 = pygame.image.load("pictures/cfrounded.png")
+image4_normal = pygame.transform.scale(image4, (150, 150))
+image4_goat =pygame.transform.scale(image4, (90, 90))
+
+#CLOSE CENTRAL VOWELS
+image5 = pygame.image.load("pictures/CCU.png")
+image5_normal = pygame.transform.scale(image5, (150, 150))
+image5_goat = pygame.transform.scale(image5, (90, 90))
+
+image6 = pygame.image.load("pictures/ccunrounded.png")
+image6_normal = pygame.transform.scale(image6, (150, 150))
+image6_goat = pygame.transform.scale(image6, (90, 90))
+
+image7 = pygame.image.load("pictures/CCR.png")
+image7_normal = pygame.transform.scale(image7, (150, 150))
+image7_goat =  pygame.transform.scale(image7, (90, 90))
+
+image8 = pygame.image.load("pictures/ccrounded.png")
+image8_normal = pygame.transform.scale(image8, (150, 150))
+image8_goat = pygame.transform.scale(image8, (90, 90))
+
+#CLOSE BACK VOWELS
+image9 = pygame.image.load("pictures/CBU.png")
+image9_normal = pygame.transform.scale(image9, (150, 150))
+image9_goat = pygame.transform.scale(image9, (90, 90))
+
+image10 = pygame.image.load("pictures/cbunrounded.png")
+image10_normal = pygame.transform.scale(image10, (150, 150))
+image10_goat = pygame.transform.scale(image10, (90, 90))
+
+image11 = pygame.image.load("pictures/CBR.png")
+image11_normal = pygame.transform.scale(image11, (150, 150))
+image11_goat =  pygame.transform.scale(image11, (90, 90))
+
+image12 = pygame.image.load("pictures/cbrounded.png")
+image12_normal = pygame.transform.scale(image12, (150, 150))
+image12_goat = pygame.transform.scale(image12, (90, 90))
+
+
+#CLOSE MID FRONT VOWELS
+image13 = pygame.image.load("pictures/CMFU.png")
+image13_normal = pygame.transform.scale(image13, (150, 150))
+image13_goat = pygame.transform.scale(image13, (90, 90))
+
+image14 = pygame.image.load("pictures/cmfunrounded.png")
+image14_normal = pygame.transform.scale(image14, (150, 150))
+image14_goat = pygame.transform.scale(image14, (90, 90))
+
+image15 = pygame.image.load("pictures/CMFR.png")
+image15_normal = pygame.transform.scale(image15, (150, 150))
+image15_goat = pygame.transform.scale(image15, (90, 90))
+
+image16 = pygame.image.load("pictures/cmfrounded.png")
+image16_normal = pygame.transform.scale(image16, (150, 150))
+image16_goat = pygame.transform.scale(image16, (90, 90))
+
+
+#CLOSE-MID CENTRAL VOWELS
+image17 = pygame.image.load("pictures/CMCU.png")
+image17_normal = pygame.transform.scale(image17, (150, 150))
+image17_goat = pygame.transform.scale(image17, (90, 90))
+
+image18 = pygame.image.load("pictures/cmcunrounded.png")
+image18_normal = pygame.transform.scale(image18, (150, 150))
+image18_goat = pygame.transform.scale(image18, (90, 90))
+
+image19 = pygame.image.load("pictures/CMCR.png")
+image19_normal = pygame.transform.scale(image19, (150, 150))
+image19_goat  = pygame.transform.scale(image19, (90, 90))
+
+image20 = pygame.image.load("pictures/cmcrounded.png")
+image20_normal = pygame.transform.scale(image20, (150, 150))
+image20_goat = pygame.transform.scale(image20, (90, 90))
+
+
+#CLOSE-MID BACK VOWELS
+image21 = pygame.image.load("pictures/CMBU.png")
+image21_normal = pygame.transform.scale(image21, (150, 150))
+image21_goat = pygame.transform.scale(image21, (90, 90))
+
+image22 = pygame.image.load("pictures/cmbunrounded.png")
+image22_normal = pygame.transform.scale(image22, (150, 150))
+image22_goat = pygame.transform.scale(image22, (90, 90))
+
+image23 = pygame.image.load("pictures/CMBR.png")
+image23_normal = pygame.transform.scale(image23, (150, 150))
+image23_goat = pygame.transform.scale(image23, (90, 90))
+
+image24 = pygame.image.load("pictures/cmbrounded.png")
+image24_normal = pygame.transform.scale(image24, (150, 150))
+image24_goat = pygame.transform.scale(image24, (90, 90))
+
+
+#OPEN-MID FRONT VOWELS
+image25 = pygame.image.load("pictures/OMFU.png")
+image25_normal = pygame.transform.scale(image25, (150, 150))
+image25_goat = pygame.transform.scale(image25, (90, 90))
+
+image26 = pygame.image.load("pictures/omfunrounded.png")
+image26_normal = pygame.transform.scale(image26, (150, 150))
+image26_goat = pygame.transform.scale(image26, (90, 90))
+
+image27 = pygame.image.load("pictures/OMFR.png")
+image27_normal = pygame.transform.scale(image27, (150, 150))
+image27_goat = pygame.transform.scale(image27, (90, 90))
+
+image28 = pygame.image.load("pictures/omfrounded.png")
+image28_normal = pygame.transform.scale(image28, (150, 150))
+image28_goat = pygame.transform.scale(image28, (90, 90))
+
+
+#OPEN-MID CENTRAL VOWELS
+image29 = pygame.image.load("pictures/OMCU.png")
+image29_normal = pygame.transform.scale(image29, (150, 150))
+image29_goat = pygame.transform.scale(image29, (90, 90))
+
+image30 = pygame.image.load("pictures/omcunrounded.png")
+image30_normal = pygame.transform.scale(image30, (150, 150))
+image30_goat = pygame.transform.scale(image30, (90, 90))
+
+image31 = pygame.image.load("pictures/OMCR.png")
+image31_normal = pygame.transform.scale(image31, (150, 150))
+image31_goat = pygame.transform.scale(image31, (90, 90))
+
+image32 = pygame.image.load("pictures/omcrounded.png")
+image32_normal = pygame.transform.scale(image32, (150, 150))
+image32_goat = pygame.transform.scale(image32, (90, 90))
+
+
+#OPEN-MID BACK VOWELS
+image33 = pygame.image.load("pictures/OMBU.png")
+image33_normal = pygame.transform.scale(image33, (150, 150))
+image33_goat = pygame.transform.scale(image33, (90, 90))
+
+image34 = pygame.image.load("pictures/ombunrounded.png")
+image34_normal = pygame.transform.scale(image34, (150, 150))
+image34_goat = pygame.transform.scale(image34, (90, 90))
+
+image35 = pygame.image.load("pictures/OMBR.png")
+image35_normal = pygame.transform.scale(image35, (150, 150))
+image35_goat = pygame.transform.scale(image35, (90, 90))
+
+image36 = pygame.image.load("pictures/ombrounded.png")
+image36_normal = pygame.transform.scale(image36, (150, 150))
+image36_goat = pygame.transform.scale(image36, (90, 90))
+
+
+#OPEN FRONT VOWELS
+image37 = pygame.image.load("pictures/OFU.png")
+image37_normal = pygame.transform.scale(image37, (150, 150))
+image37_goat = pygame.transform.scale(image37, (90, 90))
+
+image38 = pygame.image.load("pictures/ofunrounded.png")
+image38_normal = pygame.transform.scale(image38, (150, 150))
+image38_goat = pygame.transform.scale(image38, (90, 90))
+
+image39 = pygame.image.load("pictures/OFR.png")
+image39_normal = pygame.transform.scale(image39, (150, 150))
+image39_goat = pygame.transform.scale(image39, (90, 90))
+
+image40 = pygame.image.load("pictures/ofrounded.png")
+image40_normal = pygame.transform.scale(image40, (150, 150))
+image40_goat = pygame.transform.scale(image40, (90, 90))
+
+
+#OPEN BACK VOWELS
+image41 = pygame.image.load("pictures/OBU.png")
+image41_normal = pygame.transform.scale(image41, (150, 150))
+image41_goat = pygame.transform.scale(image41, (90, 90))
+
+image42 = pygame.image.load("pictures/obunrounded.png")
+image42_normal = pygame.transform.scale(image42, (150, 150))
+image42_goat = pygame.transform.scale(image42, (90, 90))
+
+image43 = pygame.image.load("pictures/OBR.png")
+image43_normal = pygame.transform.scale(image43, (150, 150))
+image43_goat = pygame.transform.scale(image43, (90, 90))
+
+image44 = pygame.image.load("pictures/obrounded.png")
+image44_normal = pygame.transform.scale(image44, (150, 150))
+image44_goat = pygame.transform.scale(image44, (90, 90))
+
+
+
+#HARDMODE VOWEL NAMES
+#the names of the pictures are the acronyms of the starting words in each variation f.i Central Front Vowel -> CFV
+image45 = pygame.image.load("pictures/CFV.png")
+image45= pygame.transform.scale(image45, (150, 150))
+image46 = pygame.image.load("pictures/CCV.png")
+image46 = pygame.transform.scale(image46, (150, 150))
+image47 = pygame.image.load("pictures/CBV.png")
+image47 = pygame.transform.scale(image47, (150, 150))
+image48= pygame.image.load("pictures/CMFV.png")
+image48 = pygame.transform.scale(image48, (150, 150))
+image49 = pygame.image.load("pictures/CMCV.png")
+image49= pygame.transform.scale(image49, (150, 150))
+image50 = pygame.image.load("pictures/CMBV.png")
+image50 = pygame.transform.scale(image50, (150, 150))
+image51 = pygame.image.load("pictures/OMFV.png")
+image51 = pygame.transform.scale(image51, (150, 150))
+image52= pygame.image.load("pictures/OMCV.png")
+image52 = pygame.transform.scale(image52, (150, 150))
+image53 = pygame.image.load("pictures/OMBV.png")
+image53= pygame.transform.scale(image53, (150, 150))
+image54 = pygame.image.load("pictures/OFV.png")
+image54 = pygame.transform.scale(image54, (150, 150))
+image55 = pygame.image.load("pictures/OBV.png")
+image55 = pygame.transform.scale(image55, (150, 150))
+
+
+# MATCH ANIMATION PICTURES
 image_match = pygame.image.load("pictures/match_animation.png")
 image_match2 = pygame.image.load("pictures/match_animation2.png")
 cover_image = pygame.image.load("pictures/cover.png")
 cover_image = pygame.transform.scale(cover_image, (150, 150))
+cover_image2 = pygame.transform.scale(cover_image, (90, 90))
 
 animation = [image_match, image_match2]
